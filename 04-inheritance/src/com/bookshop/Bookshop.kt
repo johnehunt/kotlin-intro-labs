9d385699a84fc727f49d5d22c962a48542cf39c4
John Hunt <john.hunt10@gmail.com>
2020-08-04 18:24:52 +0100

    Fixed typo in string

--- 04-inheritance/src/com/bookshop/Bookshop.kt
+++ 04-inheritance/src/com/bookshop/Bookshop.kt
@@ -15,7 +15,7 @@ object Bookshop {
         val author = Author("Pete Smith")
         val address = Address(10, "High Street", "Salisbury", "Wiltshire", "SL10 34D")
         val publisher = Publisher("Tech Books Publishing Ltd.", address)
-        book = Book("Scala Unleashed", 15.95, author, publisher)
+        book = Book("Kotlin Unleashed", 15.95, author, publisher)
     }
 
     fun printDetails() {
