d89a4da4bb04b797a144e43ce5ba6275a15bcecd
John Hunt <john.hunt10@gmail.com>
2020-08-04 18:24:41 +0100

    Fixed typo in string

--- 02-bookshop-classes/src/com/bookshop/BookshopApp.kt
+++ 02-bookshop-classes/src/com/bookshop/BookshopApp.kt
@@ -6,7 +6,7 @@ fun main() {
     val author = Author("Pete Smith")
     val address = Address(10, "High Street", "Salisbury", "Wiltshire", "SL10 34D")
     val publisher = Publisher("Tech Books Publishing Ltd.", address)
-    val book = Book("Scala Unleashed", 15.95, author, publisher)
+    val book = Book("Kotlin Unleashed", 15.95, author, publisher)
     println("Book: $book")
 
     println("\nCalculating the Sales Discount price")
