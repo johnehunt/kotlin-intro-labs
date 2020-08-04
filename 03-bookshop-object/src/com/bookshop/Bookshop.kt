69a1047141b39fe63c504bdcdb3fd53a46cd1d8d
John Hunt <john.hunt10@gmail.com>
2020-08-04 17:07:56 +0100

    Sample solution to lab3

---
+++ 03-bookshop-object/src/com/bookshop/Bookshop.kt
@@ -0,0 +1,27 @@
+package com.bookshop
+
+object Bookshop {
+
+    val name = "John's Bookshop"
+    val address = Address(26,
+            "Main Street",
+            "Bath",
+            "BANES",
+            "BA1 3ZZ")
+
+    val book: Book
+
+    init {
+        val author = Author("Pete Smith")
+        val address = Address(10, "High Street", "Salisbury", "Wiltshire", "SL10 34D")
+        val publisher = Publisher("Tech Books Publishing Ltd.", address)
+        book = Book("Scala Unleashed", 15.95, author, publisher)
+    }
+
+    fun printDetails() {
+        println("Name: $name")
+        println("Address: $address")
+        println("Books: $book")
+    }
+
+}
