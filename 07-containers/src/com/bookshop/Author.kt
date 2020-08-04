477f28482ea15d90a66dc3c9e9872c0568bd4665
John Hunt <john.hunt10@gmail.com>
2020-08-04 18:25:24 +0100

    Added solutions to lab 7

---
+++ 07-containers/src/com/bookshop/Author.kt
@@ -0,0 +1,3 @@
+package com.bookshop
+
+data class Author(val name: String)
---
+++ 07-containers/src/com/bookshop/BookshopApp.kt
@@ -0,0 +1,12 @@
+package com.bookshop
+
+fun main() {
+    println("Welcome to the Bookshop!")
+
+    Bookshop.printDetails()
+
+    println("\nCalculating the Sales Discount price")
+    Bookshop.book.salesDiscount = 0.10
+    println("Sale price of book: ${Bookshop.book.calculateSalePrice()}")
+
+}
