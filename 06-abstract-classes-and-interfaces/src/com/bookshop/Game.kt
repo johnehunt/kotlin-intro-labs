b3c261639827b2b51468906f2c1737d2031796ac
John Hunt <john.hunt10@gmail.com>
2021-01-11 10:44:06 +0000

    Fixed typos in property name

--- 06-abstract-classes-and-interfaces/src/com/bookshop/Game.kt
+++ 06-abstract-classes-and-interfaces/src/com/bookshop/Game.kt
@@ -4,10 +4,10 @@ class Game(
     _title: String,
     _price: Double,
     override val author: Author,
-    val manufacter: String
+    val manufacturer: String
 ) : Product(_title, _price), Authors {
 
     override fun toString()=
-        "Game(${super.toString()}, author=$author, manufacter=$manufacter)"
+        "Game(${super.toString()}, author=$author, manufacturer=$manufacturer)"
 
 }
