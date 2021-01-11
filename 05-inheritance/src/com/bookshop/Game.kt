4e213bf52b62e29217fcf00640a95217f3f4309b
John Hunt <john.hunt10@gmail.com>
2021-01-11 10:18:10 +0000

    Added a Game type

---
+++ 05-inheritance/src/com/bookshop/Game.kt
@@ -0,0 +1,11 @@
+package com.bookshop
+
+class Game(
+    _title: String,
+    _price: Double,
+    val author: Author, val manufacter: String
+) : Product(_title, _price) {
+    override fun toString(): String {
+        return "Game(${super.toString()}, author=$author, manufacter=$manufacter)"
+    }
+}
