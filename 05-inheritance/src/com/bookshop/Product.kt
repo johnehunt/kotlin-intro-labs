bd652046a9f20b00f7bf32ca19c19af07d0f8feb
John Hunt <john.hunt10@gmail.com>
2021-01-19 12:31:14 +0000

    Minor change to layout

--- 05-inheritance/src/com/bookshop/Product.kt
+++ 05-inheritance/src/com/bookshop/Product.kt
@@ -4,21 +4,19 @@ package com.bookshop
  * Super class of all types of products in the shop
  */
 open class Product(val title: String,
-              val price: Double) {
+                   val price: Double) {
 
     var salesDiscount = 0.0
 
     var numberHeld = 0
-        get() {
-            return field
-        }
+        get() = field
         set(value) {
             if (value >= 0) {
                 field = value
             }
         }
 
-    fun calculateSalePrice(): Double {
+    open fun calculateSalePrice(): Double {
         var price = 0.0
         val discountAmount = this.price * salesDiscount
         price = this.price - discountAmount
