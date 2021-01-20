46e7f532a61a621805043f0557e3231391929833
John Hunt <john.hunt10@gmail.com>
2021-01-20 12:15:51 +0000

    Tidying layout

--- 07-data-and-sealed/src/com/bookshop/Genre.kt
+++ 07-data-and-sealed/src/com/bookshop/Genre.kt
@@ -2,18 +2,22 @@ package com.bookshop
 
 sealed class Genre
 
-object Fiction: Genre() {
-    override fun toString()= "Fiction"
+object Fiction : Genre() {
+    override fun toString() = "Fiction"
 }
-object Technical: Genre() {
-    override fun toString()= "Technical"
+
+object Technical : Genre() {
+    override fun toString() = "Technical"
 }
-object History: Genre() {
-    override fun toString()= "History"
+
+object History : Genre() {
+    override fun toString() = "History"
 }
-object Detective: Genre(){
-    override fun toString()= "Detective"
+
+object Detective : Genre() {
+    override fun toString() = "Detective"
 }
-object Biography: Genre(){
-    override fun toString()= "Biography"
+
+object Biography : Genre() {
+    override fun toString() = "Biography"
 }
